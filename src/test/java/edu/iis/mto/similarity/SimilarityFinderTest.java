d8605b9a09ce4f3901ef6a150b7d1af4d2babef2
Natalia Ciosek <n_ciosek@wp.pl>
2019-03-31 10:55:51 +0200

    SimilarityFinderTestGoodValue

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SimilarityFinderTest {
+
+    @Test public void SimilarityFinderTestGoodValue(){
+        SimilarityFinder finder=new SimilarityFinder();
+        int seq1[]={1,2,3,4,5,6};
+        int seq2[]={5,4,6,7,8,9};
+        Assertions.assertEquals(0.5,finder.calculateJackardSimilarity(seq1,seq2));
+    }
+}
