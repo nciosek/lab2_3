353ced50ffb2d70ecf6d4f69d1fcf0c81f82d86f
Natalia Ciosek <n_ciosek@wp.pl>
2019-04-01 22:12:36 +0200

    SimilarityFinderTestValueEqualZeroWithDifferentSequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -7,23 +7,30 @@ import org.junit.jupiter.api.Test;
 class SimilarityFinderTest {
 
     @Test public void SimilarityFinderTestGoodValue(){
-        SimilarityFinder finder=new SimilarityFinder();
-        int seq1[]={1,2,3,4,5,6};
-        int seq2[]={5,4,6,7,8,9};
-        Assertions.assertEquals(0.5,finder.calculateJackardSimilarity(seq1,seq2));
+        SimilarityFinder similarityFinder = new SimilarityFinder();
+        int seq1[] = {1,2,3,4,5,6};
+        int seq2[] = {5,4,6,7,8,9};
+        Assertions.assertEquals(0.5, similarityFinder.calculateJackardSimilarity(seq1,seq2));
     }
 
     @Test public void SimilarityFinderTestSWithSequencesLenZero(){
-        SimilarityFinder finder=new SimilarityFinder();
-        int seq1[]={};
-        int seq2[]={};
-        Assertions.assertEquals(1.0,finder.calculateJackardSimilarity(seq1,seq2));
+        SimilarityFinder similarityFinder = new SimilarityFinder();
+        int seq1[] = {};
+        int seq2[] = {};
+        Assertions.assertEquals(1.0, similarityFinder.calculateJackardSimilarity(seq1,seq2));
     }
 
     @Test public void SimilarityFinderTestValueEqualOneWithTheSameSequences(){
-        SimilarityFinder finder = new SimilarityFinder();
-        int seq1[]={1,2,3};
-        int seq2[]={1,2,3};
-        Assertions.assertEquals(1.0, finder.calculateJackardSimilarity(seq1,seq2));
+        SimilarityFinder similarityFinder = new SimilarityFinder();
+        int seq1[] = {1,2,3};
+        int seq2[] = {1,2,3};
+        Assertions.assertEquals(1.0, similarityFinder.calculateJackardSimilarity(seq1,seq2));
+    }
+
+    @Test public void SimilarityFinderTestValueEqualZeroWithDifferentSequences(){
+        SimilarityFinder similarityFinder = new SimilarityFinder();
+        int seq1[] = {1,2,3};
+        int seq2[] = {6,7,9};
+        Assertions.assertEquals(0.0, similarityFinder.calculateJackardSimilarity(seq1,seq2));
     }
 }
